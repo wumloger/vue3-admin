809fcbea80493696d9670b1395d2197e9a879cb1
ray <656482519@qq.com>
2023-11-13 17:03:48 +0800

    登录鉴权解决方案

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+import './permission'
 
 // 导入全局样式
 import './styles/index.scss'
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
 import { createStore } from 'vuex'
 import user from './modules/user.js'
+import getters from './getters'
 
 export default createStore({
   state: {
-  },
-  getters: {
   },
   mutations: {
   },
   actions: {
   },
+  getters,
   modules: {
     user
   }
