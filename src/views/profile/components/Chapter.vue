c14a42dc8921dafacb9a6e6d8e926dac542478db
ray <656482519@qq.com>
2023-11-13 23:35:08 +0800

    章节模块开发

--- src/views/profile/components/Chapter.vue
+++ src/views/profile/components/Chapter.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-
-    </div>
+  <el-timeline>
+    <el-timeline-item v-for="item in list" :key="item.id" :timestamp="item.timestamp" placement="top">
+      <el-card>
+        <h4>{{ item.content }}</h4>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import chapterList from '@/constant/chapter.json'
+const list = chapterList.list
+console.log(list)
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+    padding-top: 40px;
+}
 </style>
