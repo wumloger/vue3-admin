f9bef88bec87a4f8b221af494cfc56fc4c56ad1b
ray <656482519@qq.com>
2023-11-13 20:06:57 +0800

    修复菜单中的残留问题

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,11 +1,12 @@
 <template>
   <!-- 一级 menu 菜单 -->
   <el-menu
-    :uniqueOpened="true"
-    default-active="2"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+  :default-active="activeMenu"
+  :background-color="$store.getters.cssVar.menuBg"
+  :text-color="$store.getters.cssVar.menuText"
+  :active-text-color="$store.getters.cssVar.menuActiveText"
+  :unique-opened="true"
+  router
   >
     <sidebar-item v-for="item in routes" :key="item.path" :route="item"></sidebar-item>
   </el-menu>
@@ -13,7 +14,7 @@
 
 <script setup>
 import SidebarItem from './SidebarItem'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import { computed } from 'vue'
 import { filterRouters, generateMenus } from '@/utils/route'
 
@@ -21,6 +22,12 @@ const router = useRouter()
 const routes = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateMenus(filterRoutes)
+})
+
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
 })
 console.log(JSON.stringify(routes.value))
 </script>
