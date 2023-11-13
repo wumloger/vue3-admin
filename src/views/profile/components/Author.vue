5117d5ff26ef22f712d45c4bf03e2fb451d4b44b
ray <656482519@qq.com>
2023-11-13 23:37:44 +0800

    作者模块开发

--- src/views/profile/components/Author.vue
+++ src/views/profile/components/Author.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-
+  <div class="author-container">
+    <div class="header">
+      <my-avatar image="https://pic.imgdb.cn/item/6523f17dc458853aef61feba.png" height="60px" width="60px"> </my-avatar>
+      <div class="header-desc">
+        <h3>吴明龙</h3>
+        <span>南京工业职业技术大学软件2242学生</span>
+      </div>
     </div>
+    <div class="info">Web 全栈开发工程师</div>
+  </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import MyAvatar from '@/components/MyAvatar/index.vue'
+import { } from 'vue'
 </script>
 
 <style lang="scss" scoped>
+.author-container {
+    .header {
+        display: flex;
 
+        .header-desc {
+            margin-left: 12px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+
+            span {
+                font-size: 14px;
+            }
+        }
+    }
+
+    .info {
+        margin-top: 16px;
+        line-height: 22px;
+        font-size: 14px;
+        text-indent: 26px;
+    }
+}
 </style>
