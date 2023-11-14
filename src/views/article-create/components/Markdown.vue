f6671d1f9b894b7021c4b2719fef03920aeaac10
ray <656482519@qq.com>
2023-11-14 15:49:33 +0800

    markdown编辑器

--- src/views/article-create/components/Markdown.vue
+++ src/views/article-create/components/Markdown.vue
@@ -1,15 +1,46 @@
 <template>
-    <div>
-
+  <div class="markdown-container">
+    <!-- 渲染区 -->
+    <div id="markdown-box"></div>
+    <div class="bottom">
+      <el-button type="primary" @click="onSubmitClick">提交</el-button>
     </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { onMounted } from 'vue'
+import MkEditor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css'
 
+// Editor实例
+let mkEditor
+// 处理离开页面切换语言导致 dom 无法被获取
+let el
+onMounted(() => {
+  el = document.querySelector('#markdown-box')
+  initEditor()
+})
+
+const initEditor = () => {
+  mkEditor = new MkEditor({
+    el,
+    height: '500px',
+    previewStyle: 'vertical'
+  })
+  mkEditor.getMarkdown()
+}
+
+const onSubmitClick = () => {
+  alert(mkEditor.getHTML())
 }
 </script>
 
 <style lang="scss" scoped>
-
+.markdown-container {
+    .bottom {
+        margin-top: 20px;
+        text-align: right;
+    }
+}
 </style>
