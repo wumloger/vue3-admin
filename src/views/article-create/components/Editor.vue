7995dd7cf1bc744fc43f5b6c61f0eedbda56e0f8
ray <656482519@qq.com>
2023-11-14 15:56:12 +0800

    富文本编辑器

--- src/views/article-create/components/Editor.vue
+++ src/views/article-create/components/Editor.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-
+  <div class="editor-container">
+    <div id="editor-box"></div>
+    <div class="bottom">
+      <el-button type="primary" @click="onSubmitClick">提交</el-button>
     </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import E from 'wangeditor'
+import { onMounted } from 'vue'
 
+// Editor实例
+let editor
+// 处理离开页面切换语言导致 dom 无法被获取
+let el
+onMounted(() => {
+  el = document.querySelector('#editor-box')
+  initEditor()
+})
+
+const initEditor = () => {
+  editor = new E(el)
+  editor.config.zIndex = 1
+  // 菜单栏提示
+  editor.config.showMenuTooltips = true
+  editor.config.menuTooltipPosition = 'down'
+  editor.create()
+}
+
+const onSubmitClick = () => {
+  alert(editor.txt.html())
 }
 </script>
 
 <style lang="scss" scoped>
-
+.editor-container {
+    .bottom {
+        margin-top: 20px;
+        text-align: right;
+    }
+}
 </style>
