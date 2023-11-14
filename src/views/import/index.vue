4e5d206d46096bf512f7bc5081941d339e938790
ray <656482519@qq.com>
2023-11-14 13:53:24 +0800

    文件选择后的数据处理

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <upload-excel></upload-excel>
+  <upload-excel :onSuccess="onSuccess"></upload-excel>
 </template>
 
 <script setup>
 import UploadExcel from '@/components/UploadExcel'
+
+/**
+ * 数据解析成功之后的回调
+ */
+const onSuccess = (excelData) => {
+  console.log(excelData)
+}
 </script>
 
 <style lang="scss" scoped></style>
